31e11aa07a4d4334e278a79dbfd92180b960f069
wobushiludahai <wobushiludahai@163.com>
2023-10-29 16:50:17 +0800

    update

--- build.py
+++ build.py
@@ -3,33 +3,16 @@
 import os
 import argparse
 
-def build_unitest():
+def unitest():
     os.system('mkdir -p build')
     os.system('cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE= ./x86Compile.cmake -DUNIT_TEST=1')
     os.system('cmake --build build')
     os.system('build/unitest/unit_tests')
-    pass
-
-def run_unitest():
-    pass
-
-def unitest(args):
-    if args == 'all':
-        build_unitest()
-        run_unitest()
-        return 0
-
-    if args == 'build':
-        build_unitest()
-        return 0
-
-    if args == 'run':
-        run_unitest()
-        return 0
+    return 0
 
 def execute_by_args(args):
     if args.unitest:
-        return unitest(args.unitest)
+        return unitest()
 
     return -1
 
@@ -38,11 +21,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='构建仓构建脚本')
 
     # 2. 添加命令行参数
-    parser.add_argument('-t', '--unitest', type=str, choices = ['all', 'build', 'run'],
-                            help='UniTest: 用gtest进行单元测试 all|build|run')
+    parser.add_argument('-t', '--unitest', action="store_true", help='UniTest: 用gtest进行单元测试')
 
-    parser.add_argument('-p', '--platform', type=str, choices = ['x86', 'arm'],
-                            help='Platform: 平台配置，交叉编译指定arm平台')
+    parser.add_argument('-p', '--platform', type=str, choices = ['x86', 'arm'], help='Platform: 平台配置，交叉编译指定arm平台')
 
     # 3. 从命令行中结构化解析参数
     args = parser.parse_args()
